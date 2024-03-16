92ad6577435477e25595e11c63d64e8457e6ee37
Derek <derek_s_burney@hotmail.com>
2024-03-16 17:41:21 -0400

    Basic grip function

--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
 func _ready():
-    var xr_interface = XRServer.find_interface("OpenXR")
-    if xr_interface and xr_interface.initialize():
-        get_viewport().use_xr = true
+	var xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.initialize():
+		get_viewport().use_xr = true
