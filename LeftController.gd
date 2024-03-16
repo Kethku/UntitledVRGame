1ce1567202b5e7154828ddad2b80999baf38ae5d
Derek <derek_s_burney@hotmail.com>
2024-03-16 17:40:32 -0400

    Basic grip function

---
+++ LeftController.gd
@@ -0,0 +1,22 @@
+extends XRController3D
+
+@onready var mesh = $Mesh
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	input_float_changed.connect(_on_input_float_changed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(global_position)
+
+
+func _on_input_float_changed(name, value):
+	print(name, value) # Replace with function body.
+	if name == "grip":
+		if value >= 0.5:
+			mesh.scale = Vector3.ONE * 2.0
+		else:
+			mesh.scale = Vector3.ONE
+	
