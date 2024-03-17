7ca1dd5545088330e41b83b904b2eb37b42495a7
Kaylee Simmons <kay@the-simmons.net>
2024-03-16 17:03:11 -0700

    add temp debug logging

--- main.gd
+++ main.gd
@@ -5,43 +5,43 @@ var port: String = ""
 var startup_action = ""
 
 func _ready():
-	var xr_interface = XRServer.find_interface("OpenXR")
-	if xr_interface and xr_interface.initialize():
-		get_viewport().use_xr = true
+    var xr_interface = XRServer.find_interface("OpenXR")
+    if xr_interface and xr_interface.initialize():
+        get_viewport().use_xr = true
 
-	SyncManager.connected.connect(_on_sync_manager_connected)
-	SyncManager.started.connect(_on_sync_manager_started)
+    SyncManager.connected.connect(_on_sync_manager_connected)
+    SyncManager.started.connect(_on_sync_manager_started)
 
-	var args = OS.get_cmdline_user_args()
-	var index = 0
-	while index < args.size():
-		var arg = args[index]
-		if arg == "--host":
-			startup_action = "host"
-		elif arg == "--join":
-			startup_action = "join"
-		elif arg == "--address":
-			index += 1
-			arg = args[index]
-			address = arg
-		elif arg == "--port":
-			index += 1
-			arg = args[index]
-			port = arg
-		index += 1
+    var args = OS.get_cmdline_user_args()
+    var index = 0
+    while index < args.size():
+        var arg = args[index]
+        if arg == "--host":
+            startup_action = "host"
+        elif arg == "--join":
+            startup_action = "join"
+        elif arg == "--address":
+            index += 1
+            arg = args[index]
+            address = arg
+        elif arg == "--port":
+            index += 1
+            arg = args[index]
+            port = arg
+        index += 1
 
-	match startup_action:
-		"host":
-			SyncManager.host(int(port))
-		"join":
-			SyncManager.join(address, int(port))
+    match startup_action:
+        "host":
+            SyncManager.host(int(port))
+        "join":
+            SyncManager.join(address, int(port))
 
 func _on_sync_manager_connected(id):
-	print("Player connected! %s" % id)
+    print("Player connected! %s" % id)
 
-	if startup_action == "host" || startup_action == "join":
-		await get_tree().create_timer(1.0).timeout
-		SyncManager.update_ready(true)
+    if startup_action == "host" || startup_action == "join":
+        await get_tree().create_timer(1.0).timeout
+        SyncManager.update_ready(true)
 
 func _on_sync_manager_started():
-	print("Game Started")
+    print("Game Started")
