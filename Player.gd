103efbf4d02c33de965e88860351bd35f5341a93
Kaylee Simmons <kay@the-simmons.net>
2024-03-17 19:50:19 -0700

    properly rollback grab state

--- Player.gd
+++ Player.gd
@@ -14,8 +14,8 @@ var local: bool
 var fake: bool
 var id: String
 
-var left_grabbed: R3DRigidBody
-var right_grabbed: R3DRigidBody
+var left_grabbed_path: NodePath = ""
+var right_grabbed_path: NodePath = ""
 
 func _ready():
     add_to_group("networked")
@@ -33,11 +33,15 @@ func networked_spawn(state):
     id = state.id
     global_transform = state.transform
 
+func load_state(state):
+    left_grabbed_path = state.left_grabbed_path
+    right_grabbed_path = state.right_grabbed_path
+
 func networked_process():
     var input = SyncManager.input(id)
     if input:
-        if Head:
-            Head.transform = input.head
+        # if Head:
+        #    Head.transform = input.head
         LeftHand.transform = input.left.transform
         LeftHand.scale = Vector3.ONE * (input.left.grip * 0.3 + 0.5)
         RightHand.transform = input.right.transform
@@ -46,7 +50,8 @@ func networked_process():
         var tree = get_tree()
         var root = tree.root
         if input.left.grip > 0.5:
-            if left_grabbed && is_instance_valid(left_grabbed):
+            if !left_grabbed_path.is_empty():
+                var left_grabbed = get_node(left_grabbed_path)
                 var delta = LeftHand.global_position - left_grabbed.global_position
                 var acceleration = grab_stiffness * grab_stiffness * delta - 2.0 * grab_damping * grab_stiffness * left_grabbed.linear_velocity()
                 acceleration += Vector3.UP * 9.81
@@ -54,18 +59,24 @@ func networked_process():
             else:
                 var intersecting_bodies = World.bodies_within_sphere(LeftHand.global_position, 0.3)
                 if intersecting_bodies.is_empty():
-                    left_grabbed = SyncManager.spawn("ball", root, ball_scene, { "transform": LeftHand.global_transform })
+                    var left_grabbed = SyncManager.spawn("ball", root, ball_scene, { "transform": LeftHand.global_transform })
+                    left_grabbed_path = left_grabbed.get_path()
                 else:
-                    left_grabbed = intersecting_bodies[0]
+                    left_grabbed_path = intersecting_bodies[0].get_path()
         else:
-            left_grabbed = null
+            left_grabbed_path = ""
             
-        if input.right.grip > 0.5:
-            if right_grabbed && is_instance_valid(right_grabbed):
-                var delta = RightHand.global_position - right_grabbed.global_position
-                right_grabbed.apply_impulse(delta * right_grabbed.mass())
-            else:
-                right_grabbed = SyncManager.spawn("ball", root, ball_scene, { "transform": RightHand.global_transform })
-        else:
-            right_grabbed = null
+        # if input.right.grip > 0.5:
+        #    if right_grabbed && is_instance_valid(right_grabbed):
+        #        var delta = RightHand.global_position - right_grabbed.global_position
+        #        right_grabbed.apply_impulse(delta * right_grabbed.mass())
+        #    else:
+        #        right_grabbed = SyncManager.spawn("ball", root, ball_scene, { "transform": RightHand.global_transform })
+        #else:
+        #    right_grabbed = null
+
+    return {
+        "left_grabbed_path": left_grabbed_path,
+        "right_grabbed_path": right_grabbed_path,
+    }
             
